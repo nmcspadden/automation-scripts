8b7398b30cba5868de8c084d8765f18ebc865b99
nmcspadden <nmcspadden@gmail.com>
2014-04-24 10:36:38 -0700

    Configured for our environment
    Moved log file to /Library/Logs/

--- autopkg/autopkg-wrapper.sh
+++ autopkg/autopkg-wrapper.sh
@@ -3,10 +3,9 @@
 # autopkg automation script which, when run with no arguments, checks current run's output against a default output and sends the output to a user if there are differences
 
 # adjust the following variables for your particular configuration
-# you should manually run the script with the initialize option if you change the recipe list, since that will change the output.
-recipe_list="AdobeFlashPlayer.munki MakeCatalogs.munki"
-mail_recipient="you@yourdomain.net"
-autopkg_user="autopkg"
+recipe_list="AdobeAcrobatProXUpdate.munki AdobeAir.munki AdobeFlashPlayer.munki Compressor-MAS.munki FinalCutPro-MAS.munki FirefoxESR.munki GarageBand-MAS.munki GoogleChrome.munki GoogleEarth.munki iMovie-MAS.munki iPhoto-MAS.munki Keynote-MAS.munki minecraftedu.munki Motion-MAS.munki Numbers-MAS.munki OracleJava7.munki Pages-MAS.munki VLC.munki unitywebplayer.munki TextWrangler.munki MakeCatalogs.munki"
+mail_recipient="nick.mcspadden@sacredsf.org"
+autopkg_user="nmcspadden"
 
 # don't change anything below this line
 
@@ -29,9 +28,9 @@ elif [ "${1}" == "initialize" ]; then
   echo "autopkg user: ${autopkg_user}"
   echo "user home dir: ${user_home_dir}"
 
-  # make sure autopkg folder exists in autopkg_user's Documents folder
-  if [ ! -d "${user_home_dir}"/Documents/autopkg ]; then
-    /bin/mkdir -p "${user_home_dir}"/Documents/autopkg
+  # make sure Logs folder exists in autopkg_user's ~/Library/ folder
+  if [ ! -d "${user_home_dir}"/Library/Logs/ ]; then
+    /bin/mkdir -p "${user_home_dir}"/Library/Logs/
   fi
 
   # run autopkg twice, once to get any updates and the second to get a log indicating nothing changed
@@ -40,12 +39,12 @@ elif [ "${1}" == "initialize" ]; then
   /usr/local/bin/autopkg run -v ${recipe_list} 2>&1
 
   $logger "autopkg initial run to saved log location"
-  echo "for this autopkg run, output will not be shown, but rather saved to default log location (${user_home_dir}/Documents/autopkg/autopkg.out"
-  /usr/local/bin/autopkg run ${recipe_list} 2>&1 > "${user_home_dir}"/Documents/autopkg/autopkg.out
+  echo "for this autopkg run, output will not be shown, but rather saved to default log location (${user_home_dir}/Library/Logs/autopkg.out"
+  /usr/local/bin/autopkg run ${recipe_list} 2>&1 > "${user_home_dir}"/Library/Logs/autopkg.out
 
   $logger "finished autopkg"
 
-elif [ ! -f "${user_home_dir}"/Documents/autopkg/autopkg.out ]; then
+elif [ ! -f "${user_home_dir}"/Library/Logs/autopkg.out ]; then
   # default log doesn't exist, so tell user to run this script in initialization mode and exit
   echo "ERROR: default log does not exist, please run this script with initialize argument to initialize the log"
   exit -1
@@ -58,7 +57,7 @@ else
   $logger "finished autopkg"
 
   # check output against the saved log and if differences exist, send current log to specified recipient
-  if [ "`diff /tmp/autopkg.out \"${user_home_dir}\"/Documents/autopkg/autopkg.out`" != "" ]; then
+  if [ "`diff /tmp/autopkg.out \"${user_home_dir}\"/Library/Logs/autopkg.out`" != "" ]; then
     # there are differences from a "Nothing downloaded, packaged or imported" run... might be an update or an error
     $logger "sending autopkg log"
     /usr/bin/mail -s "autopkg log" ${mail_recipient}  < /tmp/autopkg.out
